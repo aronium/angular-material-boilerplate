ead62275329e9ae9c77eb4355ecfeadce17e522a
Bosko Kukanjac <bosko.kukanjac@ghcpro.com>
2017-10-15 18:05:33 +0200

    Mobile view - hide nav on route change

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+
+import { MatSidenav } from '@angular/material';
+
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html'
 })
 export class MainLayoutComponent implements OnInit {
+  @ViewChild('sidenav') sidenav:MatSidenav;
   
   sideNavMode: string;
   isSideNavOpen: boolean = false;
-  
-  constructor() { }
+
+  constructor(private router: Router) { }
+
+  private isLargeScreen() {
+    return (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) > 720;
+  }
 
   ngOnInit() {
-    let isLarge = this.isLargeScreen();
-    this.sideNavMode = isLarge ? 'side' : 'over';
-    this.isSideNavOpen = isLarge;
-  }
+    let isLargeScreen = this.isLargeScreen();
+    this.sideNavMode = isLargeScreen ? 'side' : 'over';
+    this.isSideNavOpen = isLargeScreen;
 
-  isLargeScreen() {
-    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (width > 720) {
-        return true;
-    } else {
-        return false;
+    // On mobile view, hide sidenav on route change
+    if (!isLargeScreen) {
+      this.router.events
+        .filter(event => event instanceof NavigationStart)
+        .subscribe((event: NavigationStart) => {
+          this.sidenav.close();
+        });
     }
   }
-
 }
